b2b3d44daf183f2a94478f5289659763df5e1302
Jay <tuanhung082018@gmail.com>
2021-02-16 17:59:24 -0800

    Add file

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    public static void main(String[] args) {
+                String name1 = "Ms. Lapan";
+                String name2 = "Mrs. Gideon";
+                String name3 = "Mr. Davis";
+                String name4 = "Ms. Palmer";
+                String name5 = "Ms. Garcia";
+                String name6 = "Mrs. Barnett";
+                String name7 = "Ms. Johannessen";
+                String name8 = "Mr. James";
+                String course1 = "English III";
+                String course2 = "Precalculus";
+                String course3 = "Music Theory";
+                String course4 = "Biotechnology";
+                String course5 = "Principles of Technology I";
+                String course6 = "Latin II";
+                String course7 = "AP US History";
+                String course8 = " Business Computer Infomation Systems";
+
+                System.out.println("+------------------------------------------------------------+");
+                System.out.println("| 1 |                          " + course1 + " |       " + name1 + " |");
+                System.out.println("| 2 |                          " + course2 + " |     " + name2 + " |");
+                System.out.println("| 3 |                         " + course3 + " |       " + name3 + " |");
+                System.out.println("| 4 |                        " + course4 + " |      " + name4 + " |");
+                System.out.println("| 5 |           " +  course5 + " |      " + name5 + " |");
+                System.out.println("| 6 |                             " + course6+ " |    " + name6 + " |");
+                System.out.println("| 7 |                        " + course7 + " | " + name7 + " |");
+                System.out.println("| 8 |" + course8+ " |       " + name8 + " |");
+                System.out.println("+------------------------------------------------------------+");
+
+
+    }
+}
